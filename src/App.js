698e4c6e70f528a45d08879d8d899a353794aeab
Diogo <drd.rclrd@gmail.com>
2019-10-04 13:19:27 -0300

    change content to page login

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import api from './services/api';
 import './App.css'
-
+import Routes from './routes';
 import logo from './assets/logo.svg';
 
 function App() {
-  const [email, setEmail ] = useState('');
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    const response = await api.post('/sessions', { email });
-
-    const { _id } = response.data;
-
-    localStorage.setItem('user', _id);
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AppCNC" />
+
       <div className="content">
-        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            type="email"
-            id="email"
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
