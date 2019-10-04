076b1de623c12781bf57d04894e48e3ee179eb15
Diogo <drd.rclrd@gmail.com>
2019-10-03 22:56:03 -0300

    added elements for session with email

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import './App.css'
+
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">Cleanup</div>
+    <div className="container">
+      <img src={logo} alt="AppCNC" />
+      <div className="content">
+        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
+        
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            type="email"
+            id="email"
+            placeholder="Seu melhor e-mail"
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
