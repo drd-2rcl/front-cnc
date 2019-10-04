54a1b2370d4c89b6b94194f5af23ae3c131d5178
Diogo <drd.rclrd@gmail.com>
2019-10-03 23:15:47 -0300

    created function to submit value

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import api from './services/api';
 import './App.css'
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail ] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(email);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AppCNC" />
       <div className="content">
         <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-        
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input 
             type="email"
             id="email"
             placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
-
           <button className="btn" type="submit">Entrar</button>
         </form>
       </div>
